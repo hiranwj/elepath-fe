839c5e7f5c3a5043db442db880b09f801cbad5b5
hiran_xdoto <hiran@xdoto.io>
2025-02-06 13:48:41 +0530

    Create the homepage

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,4 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx"), route("login", "routes/login.tsx"), route("register", "routes/register.tsx")] satisfies RouteConfig;
+export default [index("routes/home.tsx"), route("login", "routes/login.tsx"), route("register", "routes/register.tsx"), route("homepage", "routes/homepage.tsx")] satisfies RouteConfig;
+// export default [index("routes/login.tsx"), route("register", "routes/register.tsx"), route("homepage", "routes/homepage.tsx")] satisfies RouteConfig;
